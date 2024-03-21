22176d985319b53f6ad538c591adcc2ec01a8f0e
Artem <temalinho@gmail.com>
2024-03-21 14:04:33 +0300

    add secure cookies

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env:{
-    SERVER_URL: process.env.SERVER_URL,
-    CLIENT_DOMAIN: process.env.CLIENT_
-  }
-};
+	env: {
+		SERVER_URL: process.env.SERVER_URL,
+		CLIENT_DOMAIN: process.env.CLIENT_DOMAIN
+	}
+}
 
-export default nextConfig;
+export default nextConfig
