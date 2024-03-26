cf1d7a5d3bddb1645847e7184be98f519fd497f3
Artem <temalinho@gmail.com>
2024-03-26 14:41:52 +0300

    1

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: '31.129.56.181' || process.env.CLIENT_DOMAIN,
+		domain: process.env.CLIENT_DOMAIN,
 		sameSite: 'none',
 		expires: 1
 	})
